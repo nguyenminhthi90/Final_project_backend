1cc8f4e6dcd993339edaeeac756ffee21d868de9
Nbinn <nhantt2001@gmail.com>
2024-04-10 16:17:04 +0700

    latest commit

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
 from flask_protobuf import flask_protobuf as FlaskProtobuf
-
+import random
 from idl.api_pb2 import OTAUpdateRequest, OTAUpdateResponse, StatusUpdateRequest, StatusUpdateResponse
 
 import statsd
@@ -25,16 +25,43 @@ def status_update():
     server_stats.incr("status_update")
     print(request.data)
     device_id = request.data["deviceId"] if "deviceId" in request.data else "unknown"
-    
+
     # Record device stats
     device_stats.incr(f"{device_id}.status_update")
     device_stats.gauge(f"{device_id}.boot_count", request.data["bootCount"])
-    device_stats.gauge(f"{device_id}.uptime_ticks", request.data["uptimeTicks"])
+    device_stats.gauge(f"{device_id}.uptime_ticks",int(request.data["uptimeTicks"]))
+    device_stats.gauge(f"{device_id}.appStats.openCount",int(request.data["appStats"]["openCount"]))
+    device_stats.gauge(f"{device_id}.appStats.ticks",int(request.data["appStats"]["ticks"]))
 
     resp = StatusUpdateResponse()
     resp.message = "Boot count: " + str(request.data["bootCount"])
     return resp.SerializeToString()
+remoteUnlock = False
 
+@app.route("/local_unlock", methods=['POST'])
+def local_unlock():
+    global remoteUnlock
+    print(request.data)
+    if(remoteUnlock == False):
+        print("local unlock")
+        random_number = random.randint(1, 255)
+        print(random_number)
+    else:
+        remoteUnlock = False
+        print("remote unlock")
+        random_number = 0
+    resp = StatusUpdateResponse()
+    resp.message = str(random_number)
+    return resp.SerializeToString()
+
+@app.route("/remote_unlock", methods=['POST'])
+def remote_unlock():
+    print(request.data)
+    global remoteUnlock
+    remoteUnlock = True
+    resp = StatusUpdateResponse()
+    resp.message = "remote unlock success"
+    return resp.SerializeToString()
 
 @app.route("/ota", methods=['POST'])
 @fb(OTAUpdateRequest)
